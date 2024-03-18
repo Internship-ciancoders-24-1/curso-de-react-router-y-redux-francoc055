c32d7f6acb5c1660c4d20b00ce99ecbfe828632c
francoc055 <castanedafranco994@gmail.com>
2024-03-18 12:33:32 -0300

    Capitulo 2 del curso de react-router-y-redux

---
+++ PlatziVideo/src/routes/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from '../containers/Home'
+import Login from '../containers/Login'
+import Register from '../containers/Register';
+import NotFound from '../containers/NotFound';
+
+function App(){
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/login' element={<Login />} />
+                <Route exact path='/register' element={<Register />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
